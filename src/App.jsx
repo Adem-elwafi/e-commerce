887724fb710ab0b4a1cd7a102cbde76fa42f4e60
Adem-elwafi <adem.elwafi.dev@gmail.com>
2025-09-29 16:49:10 +0100

    shop updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
-import Shop from './pages/shop';
+import Shop from "./pages/Shop";
+
 
 function App() {
   return (
